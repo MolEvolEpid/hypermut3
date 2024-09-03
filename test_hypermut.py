e7edefc56235da443633e8b8ca65c8ab8d211386
Zena Lapp <zenalapp@gmail.com>
2024-09-03 11:20:59 -0600

    Add a few more tests

--- test_hypermut.py
+++ test_hypermut.py
@@ -2,14 +2,19 @@ import pytest
 
 from hypermut import *
 
+iupac_dict = {"A": ["A"], "C": ["C"], "G": ["G"], "T": ["T"],
+                "R": ["A", "G"], "Y": ["C", "T"], "S": ["G", "C"], "W": ["A", "T"], "K": ["G", "T"], "M": ["A", "C"],
+                "B": ["C", "G", "T"], "D": ["A", "G", "T"], "H": ["A", "C", "T"], "V": ["A", "C", "G"],
+                "N": ["A", "C", "G", "T"], "-": ["-"]}
+
 def test_check_chars():
   check_chars('ABCD', list('ABCD'), 'Error')
   with pytest.raises(ValueError): 
     check_chars('ABCD', list('ABC'), 'Error')
 
 def test_check_width():
-  check_width('RD', 'test')
-  check_width('A', 'test')
+  assert check_width('RD', 'test') is None
+  assert check_width('A', 'test') is None
   with pytest.raises(ValueError): 
     check_width('G|TT', 'test')
   with pytest.raises(ValueError): 
@@ -27,6 +32,17 @@ def test_check_input_patterns():
     check_input_patterns('GT','A','.','RD', iupac_dict)
   with pytest.raises(ValueError): 
     check_input_patterns('G','A','.','RD|RD', iupac_dict)
+  with warnings.catch_warnings(record=True) as w:
+    check_input_patterns('G','R','.','RD', iupac_dict)
+    assert len(w) == 1
+    assert issubclass(w[-1].category, UserWarning)
+    assert "Mutation from and to have overlapping bases" in str(w[-1].message)
+
+def test_check_partial_enforce():
+  assert check_partial_enforce('partial', 'D') is None
+  with pytest.raises(ValueError): 
+    check_partial_enforce('partial', 'A')
+
 
 # def test_get_potential_matches():
 #   assert [(m.start(0), m.end(0)) for m in get_potential_matches('TGT', 0, None, re.compile('[-]*[G][-]*', re.IGNORECASE))] == [(1,2)]
@@ -37,10 +53,9 @@ def test_check_input_patterns():
 #   assert [(m.start(0), m.end(0)) for m in get_potential_matches('AGT', 0, 2, re.compile('(?<=)[-]*([ACGTRYBDHVNWSKM])[-]*(?=[-]*[G][-]*[-]*[T][-]*)', re.IGNORECASE))] == []
 #   assert [(m.start(0), m.end(0)) for m in get_potential_matches('AGGG', 0, 3, re.compile('(?<=)[-]*([ACGTRYBDHVNWSKM])[-]*(?=[-]*[G][-]*[-]*[G][-]*)', re.IGNORECASE))] == [(0, 1)]
 
-iupac_dict = {"A": ["A"], "C": ["C"], "G": ["G"], "T": ["T"],
-                "R": ["A", "G"], "Y": ["C", "T"], "S": ["G", "C"], "W": ["A", "T"], "K": ["G", "T"], "M": ["A", "C"],
-                "B": ["C", "G", "T"], "D": ["A", "G", "T"], "H": ["A", "C", "T"], "V": ["A", "C", "G"],
-                "N": ["A", "C", "G", "T"], "-": ["-"]}
+def test_compile_re():
+  assert compile_re('A', iupac_dict) == re.compile('[A]', re.IGNORECASE)
+  assert compile_re('R', iupac_dict) == re.compile('[AG]', re.IGNORECASE)
 
 def test_compute_context_prop():
   assert compute_context_prop('GCGT', 'ACGT', ['.'], 'D', iupac_dict) == 1
@@ -62,6 +77,9 @@ def test_compute_context_prop():
   assert compute_context_prop('GG', 'N', ['Y'], 'D', iupac_dict) == 0.5
   assert compute_context_prop('GG', 'NN', ['RR'], 'D', iupac_dict) == 0.25
   assert compute_context_prop('GG', 'NN', ['YY', 'RY', 'YR'], 'D', iupac_dict) == 0.75
+
+def test_slice_seq():
+  assert slice_seq('AC-TG', 3, 2, 'upstream', True) == ['C','-']
 
 def test_find_match_weight():
   assert find_match_weight('GGG', 'AGT', 0, 1, 'A', ['.'], ['RD'], 'D', iupac_dict, 'strict', False) == (1,1,0,0)
@@ -117,11 +135,12 @@ def test_find_match_weight():
   #assert find_match_weight('AAGT', 1, 2, 'A', ['A'], ['RD', 'RD'], iupac_dict, 'strict', False) == (1,1,0,0) # wrong but incorrect context caught earlier in script
   assert find_match_weight('GGGG', 'ATGT', 1, 2, 'A', ['A'], ['RD', 'AC'], 'D', iupac_dict, 'strict', False) == (1,0,0,0)
   assert find_match_weight('GGGG', 'AAAC', 1, 2, 'A', ['A'], ['RD', 'AC'], 'D', iupac_dict, 'strict', False) == (1,1,0,0)
-
   assert find_match_weight('GGG', 'ANN', 0, 1, 'A', ['.'], ['RR'], 'D', iupac_dict, 'partial', False) == (0.25,0.25,0.75,0.75)
   assert find_match_weight('GGG', 'ANN', 0, 1, 'A', ['.'], ['YY', 'YR', 'RY'], 'D', iupac_dict, 'partial', False) == (0.75,0.75,0.25,0.25)
   assert find_match_weight('GGG', 'NAN', 1, 2, 'A', ['R'], ['R'], 'D', iupac_dict, 'partial', False) == (0.25,0.25,0.75,0.75)
   assert find_match_weight('GGG', 'NAN', 1, 2, 'A', ['Y'], ['Y'], 'D', iupac_dict, 'partial', False) == (0.25,0.25,0.75,0.75)
+  assert find_match_weight('GGG', 'AGT', 2, 3, 'A', ['RD'], ['.'], 'A', iupac_dict, 'strict', False) == (1,0,0,0)
+  assert find_match_weight('GGG', 'AGT', 2, 3, 'A', ['RD'], ['.'], 'B', iupac_dict, 'strict', False) == (1,0,0,0)
 
 def test_summarize_matches():
   assert summarize_matches('GGTT', 'AGTT', 0, None,
@@ -132,7 +151,23 @@ def test_summarize_matches():
                            re.compile('G'), 'D',
                            'A', ['.'], ['RD'],
                            iupac_dict, 'strict', False, 1, 'test', None) == (1,1,0,0)
+  assert summarize_matches('GAGAT', 'A-G-T', 0, 1,
+                           re.compile('G'), 'D',
+                           'A', ['.'], ['RD'],
+                           iupac_dict, 'strict', False, 1, 'test', None) == (1,1,0,0)
+  assert summarize_matches('GAGAT', 'A-G-T', 0, 0,
+                           re.compile('G'), 'D',
+                           'A', ['.'], ['RD'],
+                           iupac_dict, 'strict', False, 1, 'test', None) == (0,0,0,0)
   
 def test_calc_fisher():
   assert calc_fisher(69,4,52,1)[1] == 0.28266930809303686
+
+def test_check_positive():
+  assert check_positive('1') == 1
+  assert check_positive(1) == 1
+  with pytest.raises(argparse.ArgumentTypeError): 
+    check_positive(-1)
+  with pytest.raises(argparse.ArgumentTypeError): 
+    check_positive('notint')
 
