d48afa63b87a277686090080626ab89b47bba65a
Zena Lapp <zenalapp@gmail.com>
2024-10-14 09:58:06 -0600

    Add new example

---
+++ example/run_hypermut.sh
@@ -0,0 +1,2 @@
+python ../hypermut.py example.fasta G A . RD -s example-strict-summary.csv -p example-strict-positions.csv -m strict
+python ../hypermut.py example.fasta G A . RD -s example-partial-summary.csv -p example-partial-positions.csv -m partial
