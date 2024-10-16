7c791438663e0f29d75af9c754a5bf2a01a92f10
Zena Lapp <zenalapp@gmail.com>
2024-10-16 11:55:39 -0600

    Add cumulative plot script

--- example/example_plot.R
+++ example/example_plot.R
@@ -1,28 +1,42 @@
-# load library
-library(tidyverse)
+# load libraries
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(stringr))
+# or just library(tidyverse)
 
 # read in positions file
-positions_strict <- read_csv('example-strict-positions.csv')
-positions_partial <- read_csv('example-partial-positions.csv')
+positions_strict <- read_csv('example-strict-positions.csv', show_col_types = FALSE)
+positions_partial <- read_csv('example-partial-positions.csv', show_col_types = FALSE)
 
 # cumulative plot (for primary)
-cum_matches_plot <- bind_rows(positions_strict %>% mutate(type = 'Strict'),
-                              positions_partial %>% mutate(type = 'Partial')) %>%
-  filter(control == 0) %>%
-  arrange(potential_mut_site) %>%
-  group_by(seq_name, type) %>%
-  mutate(cum_potential = cumsum(prop_control),
+cum_matches_plot <- bind_rows(positions_strict %>% mutate(mode = 'Strict'),
+                              positions_partial %>% mutate(mode = 'Partial')) %>%
+  # arrange by position
+  arrange(potential_mut_site) |>
+  # group by sequence, context, and match mode
+  group_by(seq_name, context, mode) |>
+  # find cumulative potential and match sites for each group
+  mutate(cum_potential = cumsum(prop_context),
          cum_match = cumsum(mut_match),
-         type = factor(type, levels = c('Strict', 'Partial'))) %>%
-  ggplot(aes(x = cum_potential, y = cum_match, col = seq_name)) +
-  facet_grid(~type) +
-  geom_line(alpha = 0.5) +
+         context = factor(str_to_sentence(context), levels = c('Primary', 'Control')),
+         mode = factor(mode, levels = c('Strict', 'Partial'))) |>
+  # plot cumulative potential and match sites for each group
+  ggplot(aes(x = cum_potential,
+             y = cum_match,
+             col = seq_name,
+             linetype = context)) +
+  facet_grid(~mode) +
+  geom_line() +
   guides(color=guide_legend(ncol=3)) +
   theme_classic() +
+  labs(x = 'Cumulative number of potential sites',
+       y = 'Cumulative number of matches',
+       linetype = 'Context',
+       col = 'Sequence') +
   theme(strip.background = element_blank(),
         strip.text = element_text(face = 'bold'),
         legend.position = 'bottom', legend.direction = 'vertical',
-  ) +
-  labs(x = 'Cumulative number of potential sites', y = 'Cumulative number of matches', col = '')
+  )
 
 ggsave('example.png', cum_matches_plot, width = 7, height = 5)
